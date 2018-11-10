c756904fc79bc877b883d5154b362d9cadc0fe45
Shahid Qureshi <shahidsabir21@hotmail.com>
2018-11-10 20:28:38 +0000

    Added code to receive and store payload from request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Create Server
 const server = http.createServer(function(req, res){
@@ -20,11 +21,24 @@ const server = http.createServer(function(req, res){
 	// Get the query string object 
 	let queryStrObj = parsedUrl.query;
 
-	// Send the response
-	res.end('Hello World!\n');
+	// Get the header 
+	let headers = req.headers;
 
-	// Log the request details
-	console.log('Request received on path : ' + trimmedPath + ' with method ' + method + ' with query string' + JSON.stringify(queryStrObj));
+	// Get the payload
+	let decoder = new StringDecoder('utf-8');
+	let buffer = '';
+
+	req.on('data', function(data){
+		buffer += decoder.write(data);
+	});
+
+	req.on('end', function(){
+		buffer += decoder.end();
+		// Send the response
+		res.end('Hello World!\n');
+		console.log('request received with this payload ' + buffer);
+	});
+
 });
 
 
