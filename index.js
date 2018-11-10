95c39bbc30aa4f0f3b3f65ded667a3d451baa389
Shahid Qureshi <shahidsabir21@hotmail.com>
2018-11-10 20:01:28 +0000

    Added URL module and some basic house keeping

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+// Dependencies
 var http = require('http');
+var url = require('url');
 
 // Create Server
 const server = http.createServer(function(req, res){
-	req.end('Hello World!')
+
+	// Get the URL and parsed it
+	let parsedUrl = url.parse(req.url, true);
+
+	// Get the path from Parsed url
+	path = parsedUrl.pathname;
+
+	// Remove '/' from either side with RegEx
+	let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+	// Get the http method
+	let method = req.method.toLowerCase();
+
+	// Get the query string object 
+	let queryStrObj = parsedUrl.query;
+
+	// Send the response
+	res.end('Hello World!\n');
+
+	// Log the request details
+	console.log('Request received on path : ' + trimmedPath + ' with method ' + method + ' with query string' + JSON.stringify(queryStrObj));
 });
 
 
